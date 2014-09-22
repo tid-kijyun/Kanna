42934f74c69d88154b35fd4e8815e94cde057e69
tid <tid.develop@gmail.com>
2014-09-22 17:03:13 +0900

    Updated main.swift.(for Swift 1.1/ add example)

--- main.swift
+++ main.swift
@@ -1,10 +1,10 @@
 import Foundation
 
-let html = "<ul><li><input type='image' name='input1' value='string1value' class='abc' /></li><li><input type='image' name='input2' value='string2value' class='def' /></li></ul><span class='spantext'><b>Hello World 1</b></span><span class='spantext'><b>Hello World 2</b></span>"
+let html = "<html><head></head><body><ul><li><input type='image' name='input1' value='string1value' class='abc' /></li><li><input type='image' name='input2' value='string2value' class='def' /></li></ul><span class='spantext'><b>Hello World 1</b></span><span class='spantext'><b>Hello World 2</b></span><a href='example.com'>example(English)</a><a href='example.co.jp'>example(JP)</a></body>"
 
 var err : NSError?
 var parser     = HTMLParser(html: html, error: &err)
-if err {
+if err != nil {
     println(err)
     exit(1)
 }
@@ -14,5 +14,12 @@ var bodyNode   = parser.body
 if let inputNodes = bodyNode?.findChildTags("b") {
     for node in inputNodes {
         println(node.contents)
+    }
+}
+
+if let inputNodes = bodyNode?.findChildTags("a") {
+    for node in inputNodes {
+        println(node.contents)
+        println(node.getAttributeNamed("href"))
     }
 }
