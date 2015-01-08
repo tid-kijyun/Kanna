fb1873896a1a0d625c23ce4c7e993cefbfbcb20f
_tid_ <tid.develop@gmail.com>
2015-01-09 05:17:26 +0900

    Cleaned sample html

--- Swift-HTML-Parser/main.swift
+++ Swift-HTML-Parser/main.swift
@@ -1,6 +1,30 @@
 import Foundation
 
-let html = "<html><head></head><body><ul><li><input type='image' name='input1' value='string1value' class='abc' /></li><li><input type='image' name='input2' value='string2value' class='def' /></li></ul><span class='spantext'><b>Hello World 1</b></span><span class='spantext'><b>Hello World 2</b></span><a href='example.com'>example(English)</a><a href='example.co.jp'>example(JP)</a><div class='val'>value1</div><div class='val'>value2</div><div class='box'><span class='dat'><strong content='2014-12-31'>2014/12/31</strong></span><h2>Hoge</h2></div></body>"
+// sample HTML
+let html =
+"<html>" +
+"<head>" +
+"</head>" +
+"<body>" +
+"  <span class='spantext'>" +
+"    <b>Hello World 1</b>" +
+"  </span>" +
+"  <span class='spantext'>" +
+"    <b>Hello World 2</b>" +
+"  </span>" +
+"  <a href='example.com'>example(English)</a>" +
+"  <a href='example.co.jp'>example(JP)</a>" +
+"  <div class='val'>value1</div>" +
+"  <div class='val'>value2</div>" +
+"  <div class='box'>" +
+"    <span class='dat'>" +
+"      <strong content='2014-12-31'>2014/12/31</strong>" +
+"    </span>" +
+"    <h2>Hoge</h2>" +
+"  </div>" +
+"</body>" +
+"</html>"
+
 
 var err : NSError?
 var parser     = HTMLParser(html: html, error: &err)
