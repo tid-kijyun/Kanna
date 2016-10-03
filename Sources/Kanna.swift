47ce22258ec986c3bd2661f849bbff2eb465ce88
tid <tid.develop@gmail.com>
2016-10-03 09:11:21 +0900

    Added the error message to the old function

--- Sources/Kanna.swift
+++ Sources/Kanna.swift
@@ -77,6 +77,34 @@ public func XML(url: URL, encoding: String.Encoding, option: ParseOption = kDefa
     return nil
 }
 
+//-------------------------------------------------------------
+// unavailable functions
+//-------------------------------------------------------------
+@available(*, unavailable, message: "Use XML(xml: String, url: String?, encoding: String.Encoding, option: ParseOption). The type of the second argument has been changed to String.Encoding from UInt.")
+public func XML(xml: String, url: String?, encoding: UInt, option: ParseOption = kDefaultXmlParseOption) -> XMLDocument? {
+    return nil
+}
+
+@available(*, unavailable, message: "Use XML(xml: String, encoding: String.Encoding, option: ParseOption). The type of the second argument has been changed to String.Encoding from UInt.")
+public func XML(xml: String, encoding: UInt, option: ParseOption = kDefaultXmlParseOption) -> XMLDocument? {
+    return nil
+}
+
+@available(*, unavailable, message: "Use XML(xml: Data, url: String?, encoding: String.Encoding, option: ParseOption). The type of the second argument has been changed to String.Encoding from UInt.")
+public func XML(xml: Data, url: String?, encoding: UInt, option: ParseOption = kDefaultXmlParseOption) ->  XMLDocument? {
+    return nil
+}
+
+@available(*, unavailable, message: "Use XML(xml: Data, encoding: String.Encoding, option: ParseOption). The type of the second argument has been changed to String.Encoding from UInt.")
+public func XML(xml: Data, encoding: UInt, option: ParseOption = kDefaultXmlParseOption) -> XMLDocument? {
+    return nil
+}
+
+@available(*, unavailable, message: "Use XML(url: URL, encoding: String.Encoding, option: ParseOption). The type of the second argument has been changed to String.Encoding from UInt.")
+public func XML(url: URL, encoding: UInt, option: ParseOption = kDefaultXmlParseOption) -> XMLDocument? {
+    return nil
+}
+
 /**
 Parse HTML
 
@@ -115,6 +143,34 @@ public func HTML(url: URL, encoding: String.Encoding, option: ParseOption = kDef
     if let data = try? Data(contentsOf: url) {
         return HTML(html: data, url: url.absoluteString, encoding: encoding, option: option)
     }
+    return nil
+}
+
+//-------------------------------------------------------------
+// unavailable functions
+//-------------------------------------------------------------
+@available(*, unavailable, message: "Use HTML(html: String, url: String?, encoding: String.Encoding, option: ParseOption). The type of the second argument has been changed to String.Encoding from UInt.")
+public func HTML(html: String, url: String?, encoding: UInt, option: ParseOption = kDefaultXmlParseOption) -> XMLDocument? {
+    return nil
+}
+
+@available(*, unavailable, message: "Use HTML(html: String, encoding: String.Encoding, option: ParseOption). The type of the second argument has been changed to String.Encoding from UInt.")
+public func HTML(html: String, encoding: UInt, option: ParseOption = kDefaultXmlParseOption) -> XMLDocument? {
+    return nil
+}
+
+@available(*, unavailable, message: "Use HTML(html: Data, url: String?, encoding: String.Encoding, option: ParseOption). The type of the second argument has been changed to String.Encoding from UInt.")
+public func HTML(html: Data, url: String?, encoding: UInt, option: ParseOption = kDefaultXmlParseOption) ->  XMLDocument? {
+    return nil
+}
+
+@available(*, unavailable, message: "Use HTML(html: Data, encoding: String.Encoding, option: ParseOption). The type of the second argument has been changed to String.Encoding from UInt.")
+public func HTML(html: Data, encoding: UInt, option: ParseOption = kDefaultXmlParseOption) -> XMLDocument? {
+    return nil
+}
+
+@available(*, unavailable, message: "Use HTML(url: URL, encoding: String.Encoding, option: ParseOption). The type of the second argument has been changed to String.Encoding from UInt.")
+public func HTML(url: URL, encoding: UInt, option: ParseOption = kDefaultXmlParseOption) -> XMLDocument? {
     return nil
 }
 
