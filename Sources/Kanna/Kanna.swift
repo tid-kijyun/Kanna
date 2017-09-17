328331f2ab0a3e7a3f9ac39b1440af42d5641fdb
Martin Prusa <martin@martinprusa.com>
2017-09-17 16:37:40 +0200

    [FEATURE] - next and previous sibling nodes

--- Sources/Kanna/Kanna.swift
+++ Sources/Kanna/Kanna.swift
@@ -234,6 +234,8 @@ public protocol XMLElement: SearchableNode {
     func addPrevSibling(_ node: XMLElement)
     func addNextSibling(_ node: XMLElement)
     func removeChild(_ node: XMLElement)
+    func nextSibling() -> XMLElement?
+    func previousSibling() -> XMLElement?
 }
 
 /**
