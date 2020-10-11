216e39d95c970183f4596728bbf8a7b33a04cc79
David Jennes <david.jennes@gmail.com>
2020-10-11 16:20:27 +0200

    Adapt Package.swift for Xcode 12

--- Package.swift
+++ Package.swift
@@ -2,6 +2,13 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
+// Starting with Xcode 12, we don't need to depend on our own libxml2 target
+#if swift(>=5.3) && !os(Linux)
+let dependencies: [Target.Dependency] = []
+#else
+let dependencies: [Target.Dependency] = ["libxml2"]
+#endif
+
 #if swift(>=5.2) && !os(Linux)
 let pkgConfig: String? = nil
 #else
